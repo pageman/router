16395dddc721070a4e664b330e0640fcc5364e5f
Mac Ignacio <mack_ignacio@yahoo.com>
2020-04-06 02:05:24 +0800

    CHORE: Add implementation for router.add()

--- sample/src/index.ts
+++ sample/src/index.ts
@@ -5,6 +5,23 @@ import { Router } from "../../";
 
 const PORT = 3333;
 const router = new Router();
+
+router.add({
+  path: "user",
+  GET: (req, res) => {
+    // Send data to the client
+    res.send({ message: "Hello, World!" });
+  },
+});
+
+router.add({
+  path: "user",
+  POST: (req, res) => {
+    // Send data to the client
+    res.send({ message: "This is a POST Request!" });
+  },
+});
+
 const server = http.createServer(router.inititalize());
 
 server.listen(PORT, () => {
