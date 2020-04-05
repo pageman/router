49439d9fbb05eb304f61dcf8e2e5ca644cd1a437
Mac Ignacio <mack_ignacio@yahoo.com>
2020-04-05 18:32:34 +0800

    FEAT: Add function to push routes in an array

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import http from "http";
 import url from "url";
+import { Route, RequestHandler, HtppMethods, UrlObject, HttpHeaders } from "./interface";
 class Router {
+  private routes: Route[] = [];
+
   constructor() {}
+
+  add(route: Route) {
+    this.routes.push(route);
+  }
 
   inititalize() {
     return (request: http.IncomingMessage, response: http.ServerResponse) => {
@@ -14,9 +21,9 @@ class Router {
       // Sanitize URL string '/request/url/' becomes 'request/url'
       path = path.replace(/^\/+|\/+$/g, "");
 
-      let query = parsedURL.query;
-      let headers = request.headers;
-      let method = request.method || "";
+      let query = parsedURL.query as UrlObject;
+      let headers = request.headers as HttpHeaders;
+      let method = request.method as HtppMethods;
       let body = "";
 
       // Get data from the REQUEST
@@ -28,7 +35,7 @@ class Router {
       // Listen to END event
       request.on("end", () => {
         // Define REQUEST Object
-        const req = { path, query, headers, method, body };
+        const req: RequestHandler = { path, query, headers, method, body };
 
         // Temporary route
         const route = (req: any, res: any) => {
