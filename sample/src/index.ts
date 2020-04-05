22e6aaa9c68dfaa6960912e4e88ee3ff163ca3a3
Mac Ignacio <mack_ignacio@yahoo.com>
2020-04-05 15:55:32 +0800

    CHORE: Replace anonymouse function with router.initialize

--- sample/src/index.ts
+++ sample/src/index.ts
@@ -1,24 +1,12 @@
 // A Simple NodeJS Server using Typescript
 
 import http from "http";
+import { Router } from "../../";
 
 const PORT = 3333;
+const router = new Router();
+const server = http.createServer(router.inititalize());
 
-const server = http.createServer(function (req, res) {
-  // SET Headers
-  res.setHeader("Content-type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // SET Status Code to HTTP 200/OK
-  res.writeHead(200);
-
-  // Define data to send on the client
-  const data = JSON.stringify({ message: "Hello world" });
-
-  // Send data to the client
-  res.end(data);
-});
-
-server.listen(PORT, function () {
-  console.log("Listening on port 1234");
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
 });
