b27c95fb187f3b20676fcbf08bea8593dd0c793a
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-26 20:03:21 +0800

    FEAT: Add methods in src

---
+++ src/methods.ts
@@ -0,0 +1,14 @@
+import http from "http";
+import { RequestMethods } from "./interfaces";
+
+/**
+ *  Generic methods from Node.js 0.10
+ * */
+const genericMethods = ["get", "post", "put", "head", "delete", "options", "patch"];
+
+/**
+ *  Current Node.js methods
+ * */
+const nodeHttpMethods = http.METHODS && http.METHODS.map((method) => method.toLowerCase());
+
+export = (nodeHttpMethods || genericMethods) as RequestMethods[];
