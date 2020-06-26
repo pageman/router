0957457caac49289c5d30ce6775846c8e97b9a13
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-27 00:20:49 +0800

    FEAT: Add init, createResponseObject and removePrefix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Routes, RequestMethod, Middleware, MethodFunction } from "./interfaces";
 import methods from "./methods";
+import http from "http";
 
 interface Router {
   get: MethodFunction;
@@ -12,6 +13,23 @@ interface Router {
 
 class Router {
   constructor(private routes: Routes = []) {}
+
+  get init() {
+    return (req: http.IncomingMessage, res: http.ServerResponse) => {
+      const url = this.removePrefix(req.url ?? "");
+      const { index, found } = this.findUrl(url);
+
+      if (!found) {
+        throw new Error("Request path doesn't exist!");
+      }
+
+      this.routes[index][url][req.method?.toLocaleLowerCase() as RequestMethod]({
+        req,
+        res: this.createResponseObject(res),
+        body: {},
+      });
+    };
+  }
 
   add(method: RequestMethod, url: string, fn: Middleware) {
     let urlIndex: number = -1;
@@ -34,6 +52,36 @@ class Router {
 
   requestHandler(options: { method: RequestMethod; url: string; fn?: Middleware }): void {
     console.log(options);
+  }
+
+  private findUrl(url: string = ""): { index: number; found: boolean } {
+    let index: number = -1;
+
+    // Add the function and the path as the key on the routes object
+    const found = this.routes.some((item: any, idx: number): boolean => {
+      index = idx;
+      return Boolean(item[url]);
+    });
+
+    return { index, found };
+  }
+
+  private createResponseObject(res: http.ServerResponse) {
+    const resFunctions = {
+      send: (value: any) => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(JSON.stringify(value));
+        res.end();
+      },
+      setHeader: () => {},
+      json: () => {},
+      html: () => {},
+    };
+    return Object.assign(res, resFunctions);
+  }
+
+  removePrefix(url: string) {
+    return url.substring(1);
   }
 }
 
