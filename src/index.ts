162d4d4d0177fd96e440570729d6331f59532301
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-26 22:57:00 +0800

    FEAT: Functionality to add route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { RequestMethod, Middleware, MethodFunction } from "./interfaces";
+import { Routes, RequestMethod, Middleware, MethodFunction } from "./interfaces";
 import methods from "./methods";
 
 interface Router {
@@ -11,14 +11,35 @@ interface Router {
 }
 
 class Router {
+  constructor(private routes: Routes = []) {}
+
+  add(method: RequestMethod, url: string, fn: Middleware) {
+    let urlIndex: number = -1;
+
+    // Add the function and the path as the key on the routes object
+    const urlFound = this.routes.some((item: any, index: number): boolean => {
+      urlIndex = index;
+      return Boolean(item[url]);
+    });
+
+    if (urlIndex >= 0 && urlFound) {
+      // add method to existing object in routes array
+      this.routes[urlIndex][url] = { [method]: fn };
+      return;
+    }
+
+    // add method new object in routes array
+    this.routes.push({ [url]: { [method]: fn } });
+  }
+
   requestHandler(options: { method: RequestMethod; url: string; fn?: Middleware }): void {
     console.log(options);
   }
 }
 
 methods.forEach((method: RequestMethod) => {
-  Router.prototype[method] = function (url: string, fn?: Middleware) {
-    return this.requestHandler({ method, url, fn });
+  Router.prototype[method] = function (url: string, fn: Middleware) {
+    return this.add(method, url, fn);
   };
 });
 
