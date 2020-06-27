318a9b2f40aa6368fe7cf713276eb3574c128222
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-27 19:40:31 +0800

    FIX: ResponseFunctions return value

--- src/response_functions.ts
+++ src/response_functions.ts
@@ -1,28 +1,26 @@
 import http from "http";
 import { AdditionalResponseObjectFunctions } from "./interfaces";
 
-export = class ResponseFunctions implements AdditionalResponseObjectFunctions {
-  constructor(private res: http.ServerResponse) {}
+export = function ResponseFunctions(res: http.ServerResponse): AdditionalResponseObjectFunctions {
+  const endResponse = (value: any) => {
+    res.write(value);
+    res.end();
+  };
 
-  send(value: any) {
-    const contentType = "Content-Type";
-    const isJSON = value instanceof Object;
-    this.res.writeHead(200, { [contentType]: isJSON ? "application/json" : "text/plain" });
-    this.endResponse(isJSON ? JSON.stringify(value) : value);
-  }
-
-  json(json: object) {
-    this.res.writeHead(200, { "Content-Type": "application/json" });
-    this.endResponse(JSON.stringify(json));
-  }
-
-  html(html: string) {
-    this.res.writeHead(200, { "Content-Type": "text/html" });
-    this.endResponse(html);
-  }
-
-  private endResponse(value: any) {
-    this.res.write(value);
-    this.res.end();
-  }
+  return {
+    send(value: any) {
+      const contentType = "Content-Type";
+      const isJSON = value instanceof Object;
+      res.writeHead(200, { [contentType]: isJSON ? "application/json" : "text/plain" });
+      endResponse(isJSON ? JSON.stringify(value) : value);
+    },
+    json(json: object) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      endResponse(JSON.stringify(json));
+    },
+    html(html: string) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      endResponse(html);
+    },
+  };
 };
