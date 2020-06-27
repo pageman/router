4f7bb7b82032525b7309fd22b91730a440c7a04d
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-27 21:04:57 +0800

    FEAT: Add RequestHelper class

---
+++ src/request.ts
@@ -0,0 +1,29 @@
+import http from "http";
+import { RequestObjectProps, RequestObject } from "./interfaces";
+
+export = class RequestHelper {
+  createRequestObject(req: http.IncomingMessage, props: RequestObjectProps): RequestObject {
+    return Object.assign(req, props);
+  }
+
+  createRequestObjectProps(params: RegExpExecArray | null, body: any, query: string): RequestObjectProps {
+    return { params: params?.groups, body, query: this.createQueryObject(query) };
+  }
+
+  private createQueryObject(query: string): { [key: string]: string } | {} {
+    const noQuestionMark = query.substring(1);
+    const queries = noQuestionMark.split("&").filter((quer) => quer);
+    const queryObject = queries.reduce((acc: { [key: string]: string }, cur): { [key: string]: string } => {
+      if (cur.includes("=")) {
+        const [key, value] = cur.split("=");
+        acc[key] = value;
+      } else {
+        acc[cur] = "";
+      }
+
+      return acc;
+    }, {});
+
+    return queryObject;
+  }
+};
