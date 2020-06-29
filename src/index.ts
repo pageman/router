9eeffb44d39535b5e4322574c1d1bf4ac16955ff
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-30 00:25:48 +0800

    FEAT: Add router namespace

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import {
-  Routes,
-  RequestMethod,
-  RouteMethodFunction,
-  MayaJSContext,
-  ResponseObject,
-  RequestObject,
-  MayaJSMiddleware,
-  ExpressMiddleware,
-  MiddlewareParams,
-} from "./interfaces";
 import response from "./response";
 import RequestHelper from "./request";
 import bodyParser from "./body_parser";
@@ -17,21 +6,112 @@ import invoker from "./middleware";
 import Url from "./url";
 import http from "http";
 
+namespace Router {
+  /**
+   * A representation of additional methods for request object
+   */
+  export interface RequestObjectProps {
+    body?: any;
+    params?: { [key: string]: string };
+    query?: { [key: string]: string };
+  }
+
+  /**
+   * A representation of additional methods for response object
+   */
+  export interface ResponseObjectProps {
+    send(args: any): void;
+    json(json: object): void;
+    html(html: string): void;
+  }
+
+  /**
+   * An object representing NodeJS Server Response with additonal
+   * methods added by the @mayajs/router api
+   */
+  export interface ResponseObject extends http.ServerResponse, ResponseObjectProps {}
+
+  /**
+   * An object representing NodeJS Incoming Message with additonal
+   * methods added by the @mayajs/router api
+   */
+  export interface RequestObject extends http.IncomingMessage, RequestObjectProps {}
+
+  /**
+   * A MayaJS object containing a request and response object.
+   * Also contains optional variables that are extracted in request and response object.
+   */
+  export interface MayaJSContext {
+    req: RequestObject;
+    res: ResponseObject;
+    body?: any;
+    params?: any;
+    query?: any;
+  }
+
+  /**
+   * A function invoking the next middleware
+   */
+  export type NextFunction = (args?: any) => void;
+
+  /**
+   * A function that accepts a 'context' object that contains all the request information
+   * and 'next' function that will execute the next middleware on the list
+   */
+  export type CallbackFunction = (obj: MayaJSContext, next?: NextFunction) => void;
+
+  /**
+   * A object params that handles request, response and error
+   */
+  export type MiddlewareParams = { req: http.IncomingMessage; res: http.ServerResponse; error?: any };
+
+  /**
+   * A function that acts as middle man to any route or another middleware function.
+   * This function will run before any route will be initialized and call the next middleware if there is any.
+   */
+  export type MayaJSMiddleware = (obj: MiddlewareParams, next: NextFunction) => void;
+
+  /**
+   * A representation of ExpressJS middleware function.
+   */
+  export type ExpressMiddleware = (req: http.IncomingMessage, res: http.ServerResponse, next: NextFunction, error: any) => void;
+
+  /**
+   * A list of http method in lower case
+   */
+  export type RequestMethod = "get" | "post" | "put" | "patch" | "delete" | "options";
+
+  /**
+   * An object to represent a route method function
+   */
+  export type RequestMethodFunction = { [method: string]: CallbackFunction };
+
+  /**
+   * A MayaJS route object
+   */
+  export type Route = { [url: string]: RequestMethodFunction };
+
+  /**
+   * A list of MayaJS Route
+   */
+  export type Routes = Route[];
+}
+
 interface Router {
-  get(path: string, fn: RouteMethodFunction): Router;
-  post(path: string, fn: RouteMethodFunction): Router;
-  put(path: string, fn: RouteMethodFunction): Router;
-  patch(path: string, fn: RouteMethodFunction): Router;
-  delete(path: string, fn: RouteMethodFunction): Router;
-  options(path: string, fn: RouteMethodFunction): Router;
+  get(path: string, fn: Router.CallbackFunction): Router;
+  post(path: string, fn: Router.CallbackFunction): Router;
+  put(path: string, fn: Router.CallbackFunction): Router;
+  patch(path: string, fn: Router.CallbackFunction): Router;
+  delete(path: string, fn: Router.CallbackFunction): Router;
+  options(path: string, fn: Router.CallbackFunction): Router;
 }
 
 class Router {
   private url: Url;
   private request: RequestHelper;
-  private middlewares: (MayaJSMiddleware | ExpressMiddleware)[];
+  private middlewares: (Router.MayaJSMiddleware | Router.ExpressMiddleware)[];
 
-  constructor(private routes: Routes = []) {
+  constructor(private routes: Router.Routes = []) {
     this.url = new Url();
     this.request = new RequestHelper();
     this.middlewares = [];
@@ -46,11 +126,11 @@ class Router {
         throw new Error("Request path doesn't exist!");
       }
 
-      const executeRoutes: MayaJSMiddleware = ({ req, res }: MiddlewareParams) =>
+      const executeRoutes: Router.MayaJSMiddleware = ({ req, res }: Router.MiddlewareParams) =>
         bodyParser(req, (body: any) => {
           const reqProps = this.request.props(params, body, query);
           const context = this.createContextObject(this.request.obj(req, reqProps), response(res));
-          const method = req.method?.toLocaleLowerCase() as RequestMethod;
+          const method = req.method?.toLocaleLowerCase() as Router.RequestMethod;
 
           this.requestHandler(index, method, key, context);
         });
@@ -59,7 +139,7 @@ class Router {
     };
   }
 
-  add(method: RequestMethod, url: string, fn: RouteMethodFunction): Router {
+  add(method: Router.RequestMethod, url: string, fn: Router.CallbackFunction): Router {
     const requestPath = this.removePrefix(url ?? "");
     const { index, found } = this.url.find(this.routes, requestPath);
 
@@ -79,7 +159,7 @@ class Router {
    *
    * @param middleware A function that will be execute before the routes
    */
-  use(middleware: MayaJSMiddleware | ExpressMiddleware | (MayaJSMiddleware | ExpressMiddleware)[]): this {
+  use(middleware: Router.MayaJSMiddleware | Router.ExpressMiddleware | (Router.MayaJSMiddleware | Router.ExpressMiddleware)[]): this {
     if (Array.isArray(middleware)) {
       this.middlewares.push(...middleware);
       return this;
@@ -90,7 +170,7 @@ class Router {
     return this;
   }
 
-  private requestHandler(index: number, method: RequestMethod, url: string, context: MayaJSContext): void {
+  private requestHandler(index: number, method: Router.RequestMethod, url: string, context: Router.MayaJSContext): void {
     const routeMethodKeys = Object.keys(this.routes[index][url]);
     const hasMethod = routeMethodKeys.includes(method);
 
@@ -105,14 +185,14 @@ class Router {
     return url.substring(1);
   }
 
-  private createContextObject(req: RequestObject, res: ResponseObject) {
+  private createContextObject(req: Router.RequestObject, res: Router.ResponseObject) {
     return { req, res, params: req.params, body: req.body, query: req.query };
   }
 }
 
 // Add all the HTTP Methods as a function to Router
-methods.forEach((method: RequestMethod) => {
-  Router.prototype[method] = function (url: string, fn: RouteMethodFunction) {
+methods.forEach((method: Router.RequestMethod) => {
+  Router.prototype[method] = function (url: string, fn: Router.CallbackFunction) {
     return this.add(method, url, fn);
   };
 });
