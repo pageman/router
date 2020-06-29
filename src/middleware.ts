f1054e09990c98f940e48acef8e47b7908613e8a
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-30 00:10:39 +0800

    FEAT: Add middleware.ts Close #5

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { MayaJSMiddleware, ExpressMiddleware } from "./interfaces";
+import http from "http";
+
+/**
+ * A helper function for invoking a list of MayaJS or ExpressJS middlewares.
+ *
+ * @param req Request object from http.IncomingMessage
+ * @param res Response object from http.ServerResponse
+ * @param middlewares List of middlewares
+ * @param error A message from the previous middleware
+ */
+export = function invoker(
+  req: http.IncomingMessage,
+  res: http.ServerResponse,
+  middlewares: (MayaJSMiddleware | ExpressMiddleware)[],
+  error?: any
+): void | Promise<void> {
+  if (!middlewares.length) return;
+
+  const mid = middlewares[0] as MayaJSMiddleware | ExpressMiddleware;
+  const next = async (error: any) => {
+    await invoker(req, res, middlewares.slice(1), error);
+  };
+
+  if (mid.length > 2) {
+    return (<ExpressMiddleware>mid)(req, res, next, error);
+  }
+
+  return (<MayaJSMiddleware>mid)({ req, res, error }, next);
+};
