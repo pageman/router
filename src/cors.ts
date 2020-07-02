87f91b35679b24c0bec93f979bd3d378de58b288
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-02 14:26:14 +0800

    FEAT: Add type checking for options.origin

--- src/cors.ts
+++ src/cors.ts
@@ -10,7 +10,15 @@ interface CorsOptions {
   maxAge?: number;
 }
 
-export = function (options?: CorsOptions): ExpressMiddleware {
+export = function (options: CorsOptions = {}): ExpressMiddleware {
+  const isString = (value: any) => typeof value === "string" || value instanceof String;
+
+  const { origin = "*" } = options;
+
+  if (isString(origin)) {
+    throw new TypeError("Options Origin is not a string!");
+  }
+
   return (req: http.IncomingMessage, res: http.ServerResponse, next: NextFunction, error: any) => {
     next();
   };
