09fe84fa91fe48b9460fed3fecd19dbf21470503
Mac Ignacio <mack_ignacio@yahoo.com>
2021-02-28 04:42:10 +0800

    FEAT: Update maya docs and description

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,24 @@ export interface MayaMiddlewares extends MayaJsMiddleware {}
  * It also cache visited routes for faster execution of route callback.
  *
  * ```
+ * import maya from "@mayajs/router";
+ * import http from "http";
+ *
  * const app = mayajs();
+ *
+ * app.add("", [
+ *   {
+ *     method: "GET",
+ *     middlewares: [],
+ *     callback: () => {
+ *       return 'Hello, World!';
+ *     },
+ *   },
+ * ]);
+ *
+ * http.createServer(app).listen(PORT, () => {
+ *  console.log(`Server listening on port ${PORT}.`);
+ * });
  * ```
  *
  * @return MayaJsRouter
