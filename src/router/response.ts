69a551bde0513ec6b65bc74d9fd899f88cd916af
Mac Ignacio <mack_ignacio@yahoo.com>
2021-02-28 02:38:39 +0800

    FEAT: Update response to handle new routes

--- src/router/response.ts
+++ src/router/response.ts
@@ -1,30 +1,31 @@
-import { ResponseObjectProps, ResponseObject } from "../";
+import { ResponseObjectProps, MayaJsResponse } from "../interface";
+import merge from "../utils/merge";
 import http from "http";
 
 function ResponseFunctions(res: http.ServerResponse): ResponseObjectProps {
   const endResponse = (value: any) => {
-    res.write(value);
+    res.write(`${value}`);
     res.end();
   };
 
   return {
-    send(value: any) {
-      const contentType = "Content-Type";
-      const isJSON = value instanceof Object;
-      res.writeHead(200, { [contentType]: isJSON ? "application/json" : "text/plain" });
-      endResponse(isJSON ? JSON.stringify(value) : value);
+    send(value: any, statusCode = 200) {
+      const isText = typeof value === "string";
+      const isError = value instanceof Error;
+      res.writeHead(isError ? 400 : statusCode, { "Content-Type": !isText ? "application/json" : "text/plain" });
+      endResponse(!isText ? JSON.stringify(isError ? { message: value.message } : value) : value);
     },
-    json(json: object) {
-      res.writeHead(200, { "Content-Type": "application/json" });
+    json(json: object, statusCode = 200) {
+      res.writeHead(statusCode, { "Content-Type": "application/json" });
       endResponse(JSON.stringify(json));
     },
-    html(html: string) {
-      res.writeHead(200, { "Content-Type": "text/html" });
+    html(html: string, statusCode = 200) {
+      res.writeHead(statusCode, { "Content-Type": "text/html" });
       endResponse(html);
     },
   };
 }
 
-export = function createResponseObject(res: http.ServerResponse): ResponseObject {
-  return Object.assign(res, ResponseFunctions(res));
-};
+export default function createResponseObject(res: http.ServerResponse): MayaJsResponse {
+  return merge(res, ResponseFunctions(res));
+}
