77c8c8034a3d4eb8043c97d28fb5090f3be6f7a5
Mac Ignacio <mack_ignacio@yahoo.com>
2021-03-29 23:09:00 +0800

    FEAT: Replace maya with index

--- src/router/maya.ts
+++
@@ -1,6 +0,0 @@
-import { app } from "./router";
-import handler from "./handler";
-import merge from "../utils/merge";
-
-// Create mayajs router by merging the handler and functions
-export default merge(handler, app);
