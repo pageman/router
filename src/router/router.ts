1a0025503f302bae26a6ce7efa0543c3c0df52c5
Mac Ignacio <mack_ignacio@yahoo.com>
2021-03-29 23:11:28 +0800

    FEAT: Update router to use functions and mapper

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,138 +1,75 @@
-import { MayaJsRoute, MayaJsContext, MayaJSRouteParams, VisitedRoutes, MethodNames, MayaRouter, Middlewares, RouteCallback, RouteMethod } from "../interface";
-import regex from "../utils/regex";
+import { MayaJsRequest, MayaJsResponse, MayaJsRoute, MethodNames, Middlewares, VisitedRoutes } from "../interface";
+import routeMapper from "../utils/mapRoutes";
+import middleware from "./middleware";
+import functions from "./functions";
 
-// Export default route object
-export const app: MayaRouter = {
-  routes: {},
-  routesWithParams: {},
-  visitedRoutes: {},
-  middlewares: [],
-} as any;
+const app: any = {};
 
-// We use '+' instead of template string '${}' because of performance gain
-// See https://stackoverflow.com/questions/6094117/prepend-text-to-beginning-of-string
-const sanitizePath = (path: string) => (path.startsWith("/") ? path : "/" + path);
+let router: any;
+let mapRoutes: any;
 
 app.add = function (routes: MayaJsRoute[]) {
   // Check if routes is an array
-  if (!Array.isArray(routes)) return this.addRouteToList(routes);
-
-  const mapRoutes = (parent = "") => (route: MayaJsRoute) => {
-    parent = parent.length > 0 ? sanitizePath(parent) : "";
-    this.addRouteToList(route, parent);
-
-    if (route?.children && route?.children.length > 0) {
-      route.children.map(mapRoutes(parent + sanitizePath(route.path)));
-    }
-  };
+  if (!Array.isArray(routes)) return router.addRouteToList(routes);
 
   // Map routes and add each route to the list
   routes.map(mapRoutes());
 };
 
-app.addRouteToList = function (route: MayaJsRoute, parent = "") {
-  const path = parent + sanitizePath(route.path);
-
-  // Check if path has params
-  const hasParams = path.includes("/:");
-
-  //  Check if path has a param and select the correct route list
-  const list = !hasParams ? "routes" : "routesWithParams";
-
-  // Initialize path if undefined
-  if (!this[list][path]) this[list][path] = {} as any;
-
-  (Object.keys(route) as MethodNames[]).map((key): void => {
-    // Define method name list
-    const methods = ["GET", "POST", "PUT", "HEAD", "DELETE", "OPTIONS", "PATCH"];
-
-    // Check if key is a method
-    if (methods.includes(key)) {
-      // Get current method
-      const current = route[key] as RouteMethod;
-
-      // Set default middlewares from route
-      let middlewares = route?.middlewares ?? [];
-
-      // Check if current method has middlewares
-      if (current?.middlewares) {
-        middlewares = [...middlewares, ...current.middlewares];
-      }
-
-      // Add route to list
-      this[list][path][key] = {
-        middlewares,
-        dependencies: [],
-        method: key,
-        regex: regex(path),
-        callback: current?.callback ?? (route[key] as RouteCallback),
-      };
-    }
-  });
-};
-
 app.init = function () {
+  const props: any = { routes: {}, routesWithParams: {}, visitedRoutes: {}, middlewares: [], context: {} };
+
+  // Add default headers
+  app["headers"] = { "X-Powered-By": "MayaJS" };
+
   // Initialize mayajs router
+  router = functions(props);
+  mapRoutes = routeMapper(router);
 };
 
 app.use = function (middleware: Middlewares) {
   // Add middleware to the list
   if (middleware) {
-    this.middlewares.push(middleware);
+    router.middlewares.push(middleware);
   }
   return this;
 };
 
-app.findRoute = function (path: string, method: MethodNames): MayaJSRouteParams | null {
-  // Check if path exist on `routes`
-  let route = this?.routes && this?.routes[path] ? this?.routes[path] : null;
+// Sends a reponse message and ending the request
+const send = async (req: MayaJsRequest, res: MayaJsResponse, parsedUrl: any) => {
+  // Get current method
+  const method = req.method as MethodNames;
 
-  // Check if path exist on `routesWithParams`
+  // Get path name
+  const routePath = parsedUrl.pathname;
+
+  // Check if path exist in visited routes or in non-param routes
+  const route = router.visitedRoute(routePath, method) || router.findRoute(routePath, method);
+
   if (!route) {
-    // Get keys from `routesWithParams` object
-    const routeWithParamsKeys = Object.keys(this.routesWithParams);
-
-    for (const key of routeWithParamsKeys) {
-      // Get current route from key
-      const current = this.routesWithParams[key];
-
-      // Test if path will pass the route path regex pattern
-      if (current[method].regex.test(path)) {
-        route = current;
-        break;
-      }
-    }
+    // Route was not found. Send back an error message
+    return res.send({ message: `Route path of '${routePath}' was not found!` }, 404);
   }
-
-  // Check if route method is same as the request
-  return route ? route[method] : null;
-};
-
-app.executeRoute = async function (path: string, route: MayaJSRouteParams, context: MayaJsContext) {
-  // Set message variable
-  let message = "";
 
   try {
-    // Try to execute route callback
-    message = await route.callback(context);
+    Object.keys(app.headers).map((key) => res.setHeader(key, app.headers[key]));
 
-    if (!this.visitedRoutes[path]) {
-      // Initialize path for caching
-      this.visitedRoutes[path] = {} as any;
-    }
+    // Create MayaJS params
+    const params = (route as VisitedRoutes).params || { ...route.regex.exec(routePath)?.groups };
 
-    if (!this.visitedRoutes[path][route.method]) {
-      // Cache path route if not on visited routes
-      this.visitedRoutes[path][route.method] = { ...route, params: context.params, query: context.query, body: context.body };
-    }
+    // Create MayaJS context
+    router.context = { req, res, query: { ...parsedUrl.query }, params, body: req?.body };
+
+    // Create a factory method for executing current route
+    const execute = async () => res.send(await router.executeRoute(routePath, route));
+
+    // Run middlewares before calling the main route callback
+    middleware([...router.middlewares, ...(route.middlewares as Middlewares[])], router.context, execute);
   } catch (error) {
-    // Catch error when running callback
-    message = error;
+    // Send error back to client
+    res.send(error);
   }
-
-  return message;
 };
 
-app.visitedRoute = function (path: string, method: MethodNames): VisitedRoutes | null {
-  return this?.visitedRoutes && this?.visitedRoutes[path] && this?.visitedRoutes[path][method] ? this?.visitedRoutes[path][method] : null;
-};
+// Create mayajs router by merging the handler and functions
+export { app, send };
