18b40d324822ecea72777095b0c4e3391bd83b5f
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-01 02:19:36 +0800

    FEAT: Add mapModules

--- src/utils/mapRoutes.ts
+++ src/utils/mapRoutes.ts
@@ -1,10 +1,37 @@
-import { MayaJsRoute } from "../interface";
+import {
+  CustomModule,
+  MayaJsRoute,
+  MayaJsRouter,
+  ModuleImports,
+  ModuleWithProviders,
+  RouterFunction,
+  RouterMapper,
+  RouterMapperFactory,
+  RouterMethods,
+  RouterProps,
+  Type,
+} from "../interface";
+import { sanitizePath } from "./helpers";
 
-// We use '+' instead of template string '${}' because of performance gain
-// See https://stackoverflow.com/questions/6094117/prepend-text-to-beginning-of-string
-const sanitizePath = (path: string) => (path.startsWith("/") ? path : "/" + path);
+const mapModules = (router: RouterProps & RouterMethods, app: MayaJsRouter, parent: string) => (imported: ModuleImports) => {
+  const args: any[] = [];
+  let tempMod;
 
-export default (router: any) => {
+  if ((imported as ModuleWithProviders)?.module) tempMod = (imported as ModuleWithProviders).module;
+  if (!imported.hasOwnProperty("module")) tempMod = imported as Type<CustomModule>;
+  if (!tempMod) return;
+  if (tempMod.name === "RouterModule") {
+    args.push(routeMapper(router, app));
+    args.push(parent);
+  }
+
+  const _module = new tempMod(...args);
+
+  _module.invoke();
+  _module.imports.map(mapModules(router, app, parent));
+};
+
+const routeMapper: RouterMapperFactory = (router: RouterFunction, app: MayaJsRouter): RouterMapper => {
   // Recursive function for mapping array of routes
   const mapRoutes = (parent = "") => (route: MayaJsRoute) => {
     // Create parent route
@@ -19,12 +46,26 @@ export default (router: any) => {
       throw new Error(`Property 'loadChildren' can't be used with 'children' in route '${routePath}'`);
     }
 
+    if (route?.controller !== undefined && route?.loadChildren !== undefined) {
+      throw new Error(`Property 'loadChildren' can't be used with 'controller' in route '${routePath}'`);
+    }
+
     // Check if route has children
     if (route?.children && route?.children.length > 0) {
       // Map children's routes
       route.children.map(mapRoutes(routePath));
     }
+
+    if (route?.loadChildren) {
+      try {
+        route?.loadChildren().then(mapModules(router, app, routePath));
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return mapRoutes;
 };
+
+export default routeMapper;
