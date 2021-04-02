ea35c4fe58a98d2c8a3f731f935f6a70fa5e90a0
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-03 03:15:52 +0800

    FEAT: Add mapDependencies, findDependency and mapProviders

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { RouterDependencies, CustomModule, ModuleProviders, Type } from "../interface";
+
 // We use '+' instead of template string '${}' because of performance gain
 // See https://stackoverflow.com/questions/6094117/prepend-text-to-beginning-of-string
 export const sanitizePath = (path: string) => {
@@ -10,4 +12,25 @@ export const sanitizePath = (path: string) => {
   }
 
   return "/" + path;
+};
+
+const mapProviders = (name: string, _module?: CustomModule | null): undefined | ModuleProviders | Function => {
+  if (!_module) return;
+  const index = _module?.providers.findIndex((item) => item.name === name);
+  return index > -1 ? _module?.providers[index] : mapProviders(name, _module.parent);
+};
+
+const findDependency = (name: string, dependencies: RouterDependencies, _module?: CustomModule | null) => {
+  if (dependencies[name]) return dependencies[name];
+
+  const provider = mapProviders(name, _module) as Type<ModuleProviders>;
+
+  if (provider) {
+    dependencies[name] = new provider();
+    return dependencies[name];
+  }
+};
+
+export const mapDependencies = (routerDep: RouterDependencies, _module?: CustomModule | null, dependencies?: any[]) => {
+  return dependencies ? dependencies.map((dep) => findDependency(dep.name, routerDep, _module) ?? null) : [];
 };
